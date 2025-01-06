d5b0d2f52082daa14186ad5faa5c704a0f93dce5
Ariiq Yusuf <yusufariiq3@gmail.com>
2025-01-06 17:24:16 +0700

    add donwload resume handler

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { User, Code, Briefcase, Award } from 'lucide-react'
-import { NavLink } from 'react-router-dom';
-import Typed from "typed.js";
+import { NavLink } from 'react-router-dom'
+import Typed from "typed.js"
+import ResumeFile from "../assets/files/cv.pdf"
 
 const Home = () => {
+  const [downloaded, setDownloaded] = useState(false);
 
   useEffect(() => {
     const options = {
       strings: ["I'm a Full Stack Engineer.", "I'm a Data Analyst"],
-      typeSpeed: 100, // Kecepatan pengetikan (ms per karakter)
-      backSpeed: 100, // Kecepatan penghapusan (ms per karakter)
-      showCursor: false, // Menampilkan kursor
-      loop: true, // Mengulang ketik
+      typeSpeed: 100,
+      backSpeed: 100,
+      showCursor: false,
+      loop: true,
     };
 
     const typed = new Typed(".full-stack-text", options);
@@ -21,6 +23,18 @@ const Home = () => {
     };
   }, []);
 
+  const handleDownload = () => {
+    if (!downloaded) {
+      const link = document.createElement("a");
+      link.href = ResumeFile;
+      link.download = "Ariiq_Yusuf_Resume.pdf";
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+      setDownloaded(true);
+    }
+  }
+
   const sections = [
     { title: 'About me', icon: User, link: '/about' },
     { title: 'Skills', icon: Code, link: '/skills' },
@@ -29,30 +43,35 @@ const Home = () => {
   ]
 
   return (
-    <div className='h-screen flex flex-col md:flex-row overflow-hidden'>
-      {/* Left section */}
-      <div className="h-full w-full md:w-1/2 bg-primary flex items-center justify-center p-8">
+    <div className='min-h-screen flex flex-col lg:flex-row'>
+      {/* Top section (Left on larger screens) */}
+      <div className="w-full lg:w-1/2 bg-primary flex items-center justify-center p-8 min-h-screen">
         <div className="max-w-2xl text-white">
           <h1 className="text-4xl md:text-6xl font-bold mb-4">Hi, I'm Ariiq Yusuf</h1>
           <div className="h-9">
-              <h2 className="text-3xl md:text-4xl font-medium full-stack-text"></h2>
+            <h2 className="text-3xl md:text-4xl font-medium full-stack-text"></h2>
           </div>
-          <button className="my-8 bg-button text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-opacity-90 transition-colors duration-300">
-            View Resume
+          <button 
+            className="my-8 bg-button hover:bg-indigo-700 text-white px-6 py-3 rounded-lg text-lg font-medium hover:bg-opacity-90 transition-colors duration-300"
+            onClick={handleDownload}
+          >
+            View resume
           </button>
         </div>
       </div>
       
-      {/* Right section */}
-      <div className="h-full w-full md:w-1/2 grid grid-cols-2 font-medium">
+      {/* Bottom section (Right on larger screens) */}
+      <div className="w-full lg:w-1/2 grid grid-cols-2 font-medium min-h-screen">
         {sections.map((section, index) => (
           <NavLink
-          to={section.link} 
-          key={section.title}
-          className={`flex flex-col items-center justify-center p-4
-          ${index % 3 === 0 ? 'bg-secondary text-third' : 'bg-third text-secondary'} group hover:bg-button transition-colors duration-500 hover:text-white`}>
-            <section.icon className='w-20 h-20 mb-2 group-hover:hidden' />
-            <p className='text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl text-center hidden group-hover:block'>
+            to={section.link} 
+            key={section.title}
+            className={`flex flex-col items-center justify-center p-4
+            ${index % 3 === 0 ? 'bg-secondary text-third' : 'bg-third text-secondary'} 
+            group hover:bg-button transition-colors duration-500 hover:text-white`}
+          >
+            <section.icon className='w-12 h-12 md:w-20 md:h-20 mb-2 group-hover:hidden' />
+            <p className='text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl text-center hidden group-hover:block'>
               {section.title}
             </p>
           </NavLink>
